a32b99af8b4a2fc25a6d37f3f2a5da398fd62490
Ian Eslick <eslick@alum.mit.edu>
2013-01-21 18:11:46 -0800

    Upgrade project.clj to Lein 2.0

--- project.clj
+++ project.clj
@@ -1,7 +1,13 @@
 (defproject handlebars-clj "0.9.1"
-  :author "Ian Eslick"
-  :license "See LICENSE file"
   :description "Implementation of handlebars templating in Clojure; can apply templates on the server side or serialize them for client-side application"
+  :author "Ian Eslick"
+  :license {:name "BSD License"
+            :url "http://github.com/eslick/handlebars-clj/blob/master/LICENSE.md"
+            :distribution :repositories}
+  :url "http://github.com/eslick/handlebars-clj"
+  :scm {:name "git"
+        :url "http://github.com/eslick/handlebars-clj"}
+  
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [hiccup "1.0.2"]]
-  :dev-dependencies [[lein-clojars "0.9.1"]])
+                 [hiccup "1.0.2"]])
